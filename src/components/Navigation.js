1b5bcd8421fae16dd3965472474d7b9403bcfec5
John Towery <kbaries@gmail.com>
2019-11-09 06:19:40 -0700

    add name to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,6 +24,11 @@ class Navigation extends React.Component {
                     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form> */}
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">John Towery</a>
+                    </li>
+                </ul>
             </div>
             </nav>
         )

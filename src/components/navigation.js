0228693f3491b28f3c8b66b27e3fe59eb17b8432
John Towery <kbaries@gmail.com>
2019-11-11 14:18:27 -0700

    correct class to className

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -3,30 +3,30 @@ import React from 'react';
 class Navigation extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="/">React on Heroku</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="/">React on Heroku</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    {/* <li class="nav-item active">
-                        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    {/* <li className="nav-item active">
+                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
                     </li> */}
-                    {/* <li class="nav-item">
-                        <a class="nav-link" href="/links">Link</a>
+                    {/* <li className="nav-item">
+                        <a className="nav-link" href="/links">Link</a>
                     </li> */}
-                    {/* <li class="nav-item">
-                        <a class="nav-link disabled" href="/disable" tabindex="-1" aria-disabled="true">Disabled</a>
+                    {/* <li className="nav-item">
+                        <a className="nav-link disabled" href="/disable" tabindex="-1" aria-disabled="true">Disabled</a>
                     </li> */}
                 </ul>
-                {/* <form class="form-inline my-2 my-lg-0 ">
-                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                {/* <form className="form-inline my-2 my-lg-0 ">
+                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
                 </form> */}
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/about">John Towery</a>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/about">John Towery</a>
                     </li>
                 </ul>
             </div>

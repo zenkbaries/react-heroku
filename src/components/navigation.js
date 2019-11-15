bf06365800df591259abae2422945439450b123a
John Towery <kbaries@gmail.com>
2019-11-14 17:13:01 -0700

    change nav from class to function

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useAuth0 } from "../react-auth0-spa";
 
-class Navigation extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="/">React on Heroku</a>
+const Navigation = () => {
+
+    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="/">React on Heroku</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -30,9 +33,30 @@ class Navigation extends React.Component {
                     </li>
                 </ul>
             </div>
-            </nav>
-        )
-    }
-}
+            <div>
+                {!isAuthenticated && (
+                    <button
+                    className="btn btn-info"
+                    onClick={() =>
+                        loginWithRedirect({})
+                    }
+                    >
+                    Log in
+                    </button>
+                )}
+
+                {isAuthenticated && 
+                    <button 
+                    className="btn btn-info"
+                    onClick={
+                        () => logout()
+                    }>
+                        Log out
+                    </button>}
+            </div>
+        </nav>
+    );
+};
+
 
 export default Navigation;

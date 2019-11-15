79413a5be4feae2ef70c9c3bb6c72b92ea3f5c69
John Towery <kbaries@gmail.com>
2019-11-14 17:14:02 -0700

    add login logout button

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -8,51 +8,34 @@ const Navigation = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="/">React on Heroku</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    {/* <li className="nav-item active">
-                        <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                    </li> */}
-                    {/* <li className="nav-item">
-                        <a className="nav-link" href="/links">Link</a>
-                    </li> */}
-                    {/* <li className="nav-item">
-                        <a className="nav-link disabled" href="/disable" tabindex="-1" aria-disabled="true">Disabled</a>
-                    </li> */}
-                </ul>
-                {/* <form className="form-inline my-2 my-lg-0 ">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                </form> */}
-                <ul className="navbar-nav">
+                <ul className="navbar-nav ml-auto">
                     <li className="nav-item">
+                        {isAuthenticated && (
                         <a className="nav-link" href="/about">John Towery</a>
+                        )}
+                    </li>
+                    <li className="nav-item">
+                    {!isAuthenticated && (
+                        <button
+                            className="btn btn-info"
+                            onClick={() =>
+                                loginWithRedirect({})
+                            }>
+                            Log in
+                        </button>
+                    )}
+                    {isAuthenticated && (
+                        <button 
+                            className="btn btn-info"
+                            onClick={
+                                () => logout()
+                            }>
+                            Log out
+                        </button>
+                    )}
                     </li>
                 </ul>
-            </div>
-            <div>
-                {!isAuthenticated && (
-                    <button
-                    className="btn btn-info"
-                    onClick={() =>
-                        loginWithRedirect({})
-                    }
-                    >
-                    Log in
-                    </button>
-                )}
-
-                {isAuthenticated && 
-                    <button 
-                    className="btn btn-info"
-                    onClick={
-                        () => logout()
-                    }>
-                        Log out
-                    </button>}
             </div>
         </nav>
     );
